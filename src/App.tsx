b4606c7bab50065edda8631fb494adff8eb431d5
Abdelrhman <abdo612200320@gmail.com>
2025-05-04 08:51:52 +0300

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,106 +1,89 @@
 import { Col, Container, Row } from "react-bootstrap";
 import "./index.css";
 function App() {
+  const Person = [
+    {
+      id: 1,
+      name: "gamal amin",
+      time: "2pm",
+      image: "src/public/1555754.png",
+    },
+    {
+      id: 2,
+      name: "Abdelrhman samy",
+      time: "4pm",
+      image: "src/public/1555754.png",
+    },
+    {
+      id: 3,
+      name: "Ahmed Ali",
+      time: "7pm",
+      image: "src/public/1555754.png",
+    },
+    {
+      id: 4,
+      name: "samy Ahmed",
+      time: "10Am",
+      image: "src/public/1555754.png",
+    },
+    {
+      id: 5,
+      name: "ramy samih",
+      time: "11Am",
+      image: "src/public/1555754.png",
+    },
+    {
+      id: 6,
+      name: "Karim mohamed",
+      time: "8pm",
+      image: "src/public/1555754.png",
+    },
+    {
+      id: 7,
+      name: "islam samier",
+      time: "9Am",
+      image: "src/public/1555754.png",
+    },
+    {
+      id: 8,
+      name: "sophy adel",
+      time: "12pm",
+      image: "src/public/1555754.png",
+    },
+  ];
   return (
     <>
       <div className="font color-body ">
         <Container className="py-5">
           <Row className="justify-content-center my-2">
             <Col sm="8" className="">
-              You have 4 appointments today
+              You have {Person.length} appointments today
             </Col>
           </Row>
           <Row className="justify-content-center">
             <Col sm="8" className="">
-              <div className="rectangle">
-                <div className="d-flex border-bottom mx-3">
-                  <img
-                    className="img-avatar p-0"
-                    src="src\public\1555754.png"
-                    alt="Profile"
-                  />
-                  <div className="px-2">
-                    <p className="d-inline fs-5 ">gamal amin</p>
-                    <p className=" fs-6">2pm</p>
-                  </div>
-                </div>
-                <div className="d-flex border-bottom mx-3">
-                  <img
-                    className="img-avatar p-0"
-                    src="src\public\1555754.png"
-                    alt="Profile"
-                  />
-                  <div className="px-2">
-                    <p className="d-inline fs-5 ">Abdelrhman samy</p>
-                    <p className=" fs-6">4pm</p>
-                  </div>
-                </div>
-                <div className="d-flex border-bottom mx-3">
-                  <img
-                    className="img-avatar p-0"
-                    src="src\public\1555754.png"
-                    alt="Profile"
-                  />
-                  <div className="px-2">
-                    <p className="d-inline fs-5 ">Ahmed Ali</p>
-                    <p className=" fs-6">7pm</p>
-                  </div>
-                </div>
-                <div className="d-flex border-bottom mx-3">
-                  <img
-                    className="img-avatar p-0"
-                    src="src\public\1555754.png"
-                    alt="Profile"
-                  />
-                  <div className="px-2">
-                    <p className="d-inline fs-5 ">samy Ahmed</p>
-                    <p className=" fs-6">10Am</p>
-                  </div>
-                </div>
-                <div className="d-flex border-bottom mx-3">
-                  <img
-                    className="img-avatar p-0"
-                    src="src\public\1555754.png"
-                    alt="Profile"
-                  />
-                  <div className="px-2">
-                    <p className="d-inline fs-5 ">ramy samih</p>
-                    <p className=" fs-6">11Am</p>
-                  </div>
-                </div>
-                <div className="d-flex border-bottom mx-3">
-                  <img
-                    className="img-avatar p-0"
-                    src="src\public\1555754.png"
-                    alt="Profile"
-                  />
-                  <div className="px-2">
-                    <p className="d-inline fs-5 ">Karim mohamed</p>
-                    <p className=" fs-6">8pm</p>
-                  </div>
-                </div>
-                <div className="d-flex border-bottom mx-3">
-                  <img
-                    className="img-avatar p-0"
-                    src="src\public\1555754.png"
-                    alt="Profile"
-                  />
-                  <div className="px-2">
-                    <p className="d-inline fs-5 ">islam samier</p>
-                    <p className=" fs-6">9Am</p>
-                  </div>
-                </div>
-                <div className="d-flex border-bottom mx-3">
-                  <img
-                    className="img-avatar p-0"
-                    src="src\public\1555754.png"
-                    alt="Profile"
-                  />
-                  <div className="px-2">
-                    <p className="d-inline fs-5 ">sophy adel</p>
-                    <p className=" fs-6">12pm</p>
-                  </div>
-                </div>
+              <div className="rectangle p-2">
+                {Person.length ? (
+                  Person.map((data) => {
+                    return (
+                      <div className="d-flex border-bottom mx-3" key={data.id}>
+                        <img
+                          className="img-avatar p-0"
+                          src={data.image}
+                          alt="Profile"
+                        />
+                        <div className="px-2">
+                          <p className="d-inline fs-5 ">{data.name}</p>
+                          <p className=" fs-6">{data.time}</p>
+                        </div>
+                      </div>
+                    );
+                  })
+                ) : (
+                  <h2 className="P-5 text-center">
+                    There are no appointments today
+                  </h2>
+                )}
               </div>
             </Col>
           </Row>
