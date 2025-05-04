feab456262fb5557db92051dadbb94f30a36cdf9
Abdelrhman <abdo612200320@gmail.com>
2025-05-04 09:21:37 +0300

    feat: create data.ts

---
+++ src/data/data.ts
@@ -0,0 +1,50 @@
+export const Person = [
+  {
+    id: 1,
+    name: "gamal amin",
+    time: "2pm",
+    image: "src/public/1555754.png",
+  },
+  {
+    id: 2,
+    name: "Abdelrhman samy",
+    time: "4pm",
+    image: "src/public/1555754.png",
+  },
+  {
+    id: 3,
+    name: "Ahmed Ali",
+    time: "7pm",
+    image: "src/public/1555754.png",
+  },
+  {
+    id: 4,
+    name: "samy Ahmed",
+    time: "10Am",
+    image: "src/public/1555754.png",
+  },
+  {
+    id: 5,
+    name: "ramy samih",
+    time: "11Am",
+    image: "src/public/1555754.png",
+  },
+  {
+    id: 6,
+    name: "Karim mohamed",
+    time: "8pm",
+    image: "src/public/1555754.png",
+  },
+  {
+    id: 7,
+    name: "islam samier",
+    time: "9Am",
+    image: "src/public/1555754.png",
+  },
+  {
+    id: 8,
+    name: "sophy adel",
+    time: "12pm",
+    image: "src/public/1555754.png",
+  },
+];
