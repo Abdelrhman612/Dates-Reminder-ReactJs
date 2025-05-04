206ec63661f43bccefe4d4878602689225a4b09a
Abdelrhman <abdo612200320@gmail.com>
2025-05-04 09:58:41 +0300

    update DatesList.tsx

--- src/components/DatesList/DatesList.tsx
+++ src/components/DatesList/DatesList.tsx
@@ -1,28 +1,35 @@
 import { Col, Row } from "react-bootstrap";
-import { RectnagleProps } from "./interFace";
+import { Person } from "./interFace";
 
-const Rectnagle = ({ rectangle }: RectnagleProps) => {
+// The Rectnagle component receives a 'person' prop, which is an array of Person objects.
+const DatesList = ({ person }: { person: Person[] }) => {
   return (
     <Row className="justify-content-center">
       <Col sm="8" className="">
         <div className="rectangle p-2">
-          {rectangle.length ? (
-            rectangle.map((data) => {
+          {/* Check if the 'person' array has data */}
+          {person.length ? (
+            // If the array has data, map through it and render each person's details
+            person.map((data) => {
               return (
-                <div className="d-flex border-bottom mx-3" key={data.id}>
+                <div key={data.id} className="d-flex border-bottom mx-3">
+                  {/* Render the person's image */}
                   <img
                     className="img-avatar p-0"
                     src={data.image}
                     alt="Profile"
                   />
                   <div className="px-2">
+                    {/* Render the person's name */}
                     <p className="d-inline fs-5 ">{data.name}</p>
+                    {/* Render the person's time */}
                     <p className=" fs-6">{data.time}</p>
                   </div>
                 </div>
               );
             })
           ) : (
+            // If the array is empty, display a message
             <h2 className="P-5 text-center">There are no appointments today</h2>
           )}
         </div>
@@ -31,4 +38,4 @@ const Rectnagle = ({ rectangle }: RectnagleProps) => {
   );
 };
 
-export default Rectnagle;
+export default DatesList;
